414c63b14eebb3996708e0fff8d8f70ee4d9fc77
aldijulfiansyah <aldiksk@gmail.com>
2020-08-18 14:26:20 +0700

    baru

--- home.php
+++ home.php
@@ -1,1 +1,21 @@
-<center><h2>Selamat datang admin</h2></center>
+
+
+<div class="text-center">
+<img src="https://s.abcnews.com/images/Entertainment/GTY_john_lennon_jt_151009_4x3_992.jpg"  class="rounded" alt="Responsive image" >
+</div>
+
+<blockquote class="blockquote">
+  <p class="mb-0">“Living is easy with eyes closed
+Misunderstanding all you see
+It's getting hard to be someone
+But it all works out
+It doesn't matter much to me.” </p>
+  <footer class="blockquote-footer">John Lennon  <cite title="Source Title"></cite></footer>
+</blockquote>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
